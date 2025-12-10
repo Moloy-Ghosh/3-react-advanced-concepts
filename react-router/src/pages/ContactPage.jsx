132abb77d20e0fb8071e1996c166d260e306adba
Moloy Ghosh <moloy.ghosh.cse@gmail.com>
2025-12-10 23:57:00 +0600

    router with slideber linked component and error page component

---
+++ react-router/src/pages/ContactPage.jsx
@@ -0,0 +1,11 @@
+
+const ContactPage =() =>{
+   return <div>
+      <p>she is so good that nowadays it doesn't seen so someone thought something different when that man gets her mind 
+         and lost with that and came back with a totally unknown behaviour. so it was necessery to meet with her. That's why 
+         she suggested that, write it on a react page component.
+      </p>
+   </div>;
+};
+
+export default ContactPage;
--- react-router/src/router/router.js
+++ react-router/src/router/router.js
@@ -1,11 +1,21 @@
 import {createBrowserRouter} from "react-router-dom";
 import Root from "../pages/RootLayout";
+import Home from "../pages/Home";
 import About from "../pages/About";
+import ContactPage from "../pages/ContactPage";
+import ErrorPage from "../pages/ErrorPage";
+import App from "../App";
 
 export const router = createBrowserRouter([
    {
       path:'/',
       element:<Root/>,
-      children:[{path:"about",element:<About/>}],
+      errorElement:<ErrorPage/>,
+      children:[
+         {index:true, element:<Home/>},
+         {path:"about",element:<About/>},
+         {path:"/contactwithme",element:<ContactPage/>},
+         {path:"/all-notes", element:<App/>}
+      ],
    }
 ]);
